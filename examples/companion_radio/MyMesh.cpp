a5452d31c63b3b172c73f2acb0a7953496c37d9b
Wessel Nieboer <wessel@weebl.me>
2026-01-08 17:02:58 +0100

    Save some more power when BLE/WiFi is disabled on the companion radio

--- examples/companion_radio/MyMesh.cpp
+++ examples/companion_radio/MyMesh.cpp
@@ -2036,3 +2036,8 @@ bool MyMesh::advert() {
     return false;
   }
 }
+
+// Check if there is pending work (packets to send)
+bool MyMesh::hasPendingWork() const {
+  return _mgr->getOutboundCount(0xFFFFFFFF) > 0;
+}
