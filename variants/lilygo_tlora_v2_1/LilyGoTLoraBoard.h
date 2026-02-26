07c5b223695672ed697662cbc13b798307164771
Wessel Nieboer <wessel@weebl.me>
2026-02-26 18:29:56 +0100

    Add Variant for T3 LoRa32 V1.6.1

--- variants/lilygo_tlora_v2_1/LilyGoTLoraBoard.h
+++ variants/lilygo_tlora_v2_1/LilyGoTLoraBoard.h
@@ -3,11 +3,15 @@
 #include <Arduino.h>
 #include <helpers/ESP32Board.h>
 
-// LILYGO T-LoRa V2.1-1.6 board with SX1276
+// LILYGO T-LoRa / T3 LoRa32 board with SX1276
 class LilyGoTLoraBoard : public ESP32Board {
 public:
   const char* getManufacturerName() const override {
+#ifdef BOARD_MANUFACTURER_NAME
+    return BOARD_MANUFACTURER_NAME;
+#else
     return "LILYGO T-LoRa V2.1-1.6";
+#endif
   }
 
   uint16_t getBattMilliVolts() override {
